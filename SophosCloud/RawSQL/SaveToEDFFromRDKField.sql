62dbbc3ae5a9968b0496b01625dabe462d084593
jacksoneyton <jackson.eyton@gmail.com>
2017-07-19 19:28:34 -0500

    Delete SaveToEDFFromRDKField.sql

--- SophosCloud/RawSQL/SaveToEDFFromRDKField.sql
+++
@@ -1,5 +0,0 @@
-UPDATE extrafielddata SET VALUE= (SELECT VALUE FROM plugin_labtech_rdk_controls_extra WHERE NAME LIKE 'Rapid Dev Kit_0_%clientid%_SophosCloud_Login_Address_Value_Text') 
-WHERE ID = '%clientid%' AND ExtraFieldID = (SELECT ExtraField.ID FROM extrafield WHERE extrafield.NAME LIKE '%SophosCloud%Login%Address%')
-
-UPDATE extrafielddata SET VALUE= (SELECT VALUE FROM plugin_labtech_rdk_controls_extra WHERE NAME LIKE 'Rapid Dev Kit_0_%clientid%_SophosCloud_Login_Pass_Value_Text') 
-WHERE ID = '%clientid%' AND ExtraFieldID = (SELECT ExtraField.ID FROM extrafield WHERE extrafield.NAME LIKE '%SophosCloud%Login%Pass%')
